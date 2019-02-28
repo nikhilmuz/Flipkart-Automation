486bd53a3a69b38d9acf63b443e83f00e8c1b6c5
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2019-03-01 02:10:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,107 @@
 import time
 import requests
 import subprocess
+import getpass
 
-#pid = raw_input ("Enter Product ID : ")
-#cookie = raw_input ("Enter Cookie header as per HTTP 1.1 : ")
+pid = raw_input("Enter Product ID (lid in URL) : ")
+phone = raw_input("Enter Phone Number : ")
+password = getpass.getpass("Enter Password : ")
 
-pid = "LSTMOBEQ98THNGR4FD5V3OUHB"
-cookie = "T=TI155132592534384785252330586872264666854936836547782723228029338613; AMCVS_17EB401053DAF4840A490D4C%40AdobeOrg=1; s_cc=true; AMCV_17EB401053DAF4840A490D4C%40AdobeOrg=-227196251%7CMCIDTS%7C17956%7CMCMID%7C87452963148642232941527437008171924540%7CMCAAMLH-1551930706%7C3%7CMCAAMB-1551930706%7C6G1ynYcLPuiQxYZrsz_pkqfLG9yMXBpb2zX5dvJdYQJzPXImdj0y%7CMCOPTOUT-1551333107s%7CNONE%7CMCAID%7CNONE; SN=2.VI704041CD18FA4F2AB8C43C8975345092.SIF6B8E8EFA4284B04B73D7D626E424C78.VS9A187847C6654F4E9CC5BAF06FBB14EA.1551326035; gpv_pn=Checkout%3ASummary; gpv_pn_t=Checkout; S=d1t18Px9nDT8VPz8/Fz8SPy0/fEGZM8ZwpNPwzjIGRKGj1Wq/TN/JTEAeXM4N385jnBq9xM5Olu8Ms2mKg8AZUrdwfw==; s_sq=flipkart-prd%3D%2526pid%253DCheckout%25253ASummary%2526pidt%253D1%2526oid%253Dfunction%252528%252529%25257B%25257D%2526oidt%253D2%2526ot%253DSUBMIT"
+xua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36 " \
+      "FKUA/website/41/website/Desktop "
+ua = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"
+
+#pid = "LSTMOBFAJB4CWKAZGPZVOYNQH"
+
+print("Logging In")
+url = "https://www.flipkart.com/api/4/user/authenticate"
+payload = "{\"loginId\":\"+91" + str(phone) + "\",\"password\":\"" + str(password) + "\"}"
+headers = {
+    'Pragma': "no-cache",
+    'X-user-agent': xua,
+    'Origin': "https://www.flipkart.com",
+    'Accept-Encoding': "gzip, deflate, br",
+    'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
+    'User-Agent': ua,
+    'Content-Type': "application/json",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Referer': "https://www.flipkart.com/",
+    'Cookie': "T=TI155137553506705890121826703635344841328671598429530214544753810704; gpv_pn=HomePage; gpv_pn_t=Homepage; AMCVS_17EB401053DAF4840A490D4C%40AdobeOrg=1; s_cc=true; AMCV_17EB401053DAF4840A490D4C%40AdobeOrg=-227196251%7CMCIDTS%7C17956%7CMCMID%7C91959474157950264714624597549432852589%7CMCAID%7CNONE%7CMCOPTOUT-1551382718s%7CNONE%7CMCAAMLH-1551980318%7C8%7CMCAAMB-1551980318%7Cj8Odv6LonN4r3an7LhD3WZrU1bUpAkFkkiY1ncBR96t2PTI; S=d1t13PyUOPz8/Rj8/Xz8bPw4/PzZG4Vgoju/YzeDpujhY6sZQbGF9kzE0z5eddPLGYrPG1NiFrK7ta5z5azKll7WbRg==; SN=2.VI7DBD8D2460E640BAB11E40ABC3B2DAFB.SIBC1DB43149BB4FA088FEC3CFA1B9FB43.VS783652EDBA3E4173949AC02384BF998B.1551375560; s_sq=%5B%5BB%5D%5D",
+    'Connection': "keep-alive",
+    'DNT': "1",
+    'cache-control': "no-cache"
+}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+if response.status_code == 400:
+    print("Incorrect Credentials")
+    exit()
+cookies = response.cookies
 
 url = "https://www.flipkart.com/api/5/checkout"
-payload = "{\"cartRequest\":{\"cartContext\":{\""+str(pid)+"\":{\"quantity\":1}}},\"checkoutType\":\"PHYSICAL\"}"
+payload = "{\"cartRequest\":{\"cartContext\":{\"" + str(pid) + "\":{\"quantity\":1}}},\"checkoutType\":\"PHYSICAL\"}"
 headers = {
-    'Cookie': str(cookie),
-    'X-user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36 FKUA/website/41/website/Desktop",
+    'X-user-agent': xua,
     'Content-Type': "application/json",
     'cache-control': "no-cache"
-    }
+}
 
+print("Trying Adding to Cart")
 while True:
-	response = requests.request("POST", url, data=payload, headers=headers)
-	res = response.json()
-        print(res['STATUS_CODE'])
-	if res['STATUS_CODE']==200:
-		break
-	print(res['ERROR_CODE'])
-       	if res['ERROR_CODE']==429:
-       		print("API Error")
-		#time.sleep(5)
+    response = requests.request("POST", url, data=payload, cookies=cookies, headers=headers)
+    res = response.json()
+    print(res['STATUS_CODE'])
+    if res['STATUS_CODE'] == 200:
+        break
+    print(res['ERROR_CODE'])
+    if res['ERROR_CODE'] == 429:
+        print("API Error")
 
+print("Added to Cart Successfully")
+
+print("Getting Payment Info")
 url = "https://www.flipkart.com/api/3/checkout/paymentToken"
 headers = {
-    'Cookie': str(cookie),
-    'X-user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36 FKUA/website/41/website/Desktop",
+    'X-user-agent': xua,
     'cache-control': "no-cache"
-    }
-response = requests.request("GET", url, data="", headers=headers)
+}
+response = requests.request("GET", url, data="", cookies=cookies, headers=headers)
 res = response.json()
 token = res['RESPONSE']['getPaymentToken']['token']
 
-url = "https://payments.flipkart.com/fkpay/api/v3/payments/captcha/"+str(token)
-querystring = {"token":token}
+print("Loading Captcha")
+url = "https://payments.flipkart.com/fkpay/api/v3/payments/captcha/" + str(token)
+querystring = {"token": token}
 headers = {'cache-control': "no-cache"}
 response = requests.request("GET", url, data="", headers=headers, params=querystring)
 res = response.json()
 cap_id = res['captcha_image']['id']
 cap_str = res['captcha_image']['image']
-subprocess.call("echo \""+str(cap_str)+"\" >captcha; base64 -d captcha >captcha.jpg; rm captcha; display captcha.jpg&", shell=True)
-captcha = raw_input("captcha?")
+subprocess.call(
+    "echo \"" + str(cap_str) + "\" >captcha; base64 -d captcha >captcha.jpg; rm captcha; display captcha.jpg&",
+    shell=True)
+captcha = raw_input("Enter Captcha Text : ")
 
+print("Processing COD Payment")
 url = "https://payments.flipkart.com/fkpay/api/v3/payments/pay"
-querystring = {"token":token,"instrument":"COD"}
-payload = "{\"payment_instrument\":\"COD\",\"token\":\""+token+"\",\"captcha_text\":{\"id\":\""+cap_id+"\",\"text\":\""+str(captcha)+"\"}}"
+querystring = {"token": token, "instrument": "COD"}
+payload = "{\"payment_instrument\":\"COD\",\"token\":\"" + token + "\",\"captcha_text\":{\"id\":\"" + cap_id + "\",\"text\":\"" + str(
+    captcha) + "\"}}"
 headers = {
     'Referer': "https://www.flipkart.com/checkout/init?otracker=hp_omu_Food%2BEssentials%2B%2526%2BNutrition%2B_1_0TNRTS53M7UY_1",
     'Origin': "https://www.flipkart.com",
     'DNT': "1",
-    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
+    'User-Agent': ua,
     'x-device-source': "web",
     'x-client-trace-id': "cjso3m4g000033169nqf3h3h2",
     'Content-Type': "application/json",
     'cache-control': "no-cache"
-    }
+}
 response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
 res = response.json()
 
+print("Processing Order")
 url = "https://www.flipkart.com/api/3/checkout/pgResponse/desktop"
 payload = res['primary_action']['parameters']
 headers = {
@@ -74,14 +112,41 @@ headers = {
     'Upgrade-Insecure-Requests': "1",
     'DNT': "1",
     'Content-Type': "application/x-www-form-urlencoded",
-    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
+    'User-Agent': ua,
     'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
     'Referer': "https://www.flipkart.com/checkout/init",
     'Accept-Encoding': "gzip, deflate, br",
     'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
-    'Cookie': str(cookie),
+    'cache-control': "no-cache"
+}
+
+print("Generating Response")
+response = requests.request("POST", url, data=payload, cookies=cookies, headers=headers)
+print(response.headers)
+
+print("Logging You Out")
+url = "https://www.flipkart.com/api/2/user/logout"
+payload = str(time.time()).replace(".", "")+"0"
+headers = {
+    'Origin': "https://www.flipkart.com",
+    'AlexaToolbar-ALX_NS_PH': "AlexaToolbar/alx-4.0.3",
+    'Accept-Language': "en-GB,en-US;q=0.9,en;q=0.8",
+    'Accept-Encoding': "gzip, deflate, br",
+    'Connection': "keep-alive",
+    'Pragma': "no-cache",
+    'X-user-agent': xua,
+    'User-Agent': ua,
+    'Content-Type': "application/json",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Referer': "https://www.flipkart.com/",
+    'DNT': "1",
     'cache-control': "no-cache"
     }
+response = requests.request("POST", url, data=payload, cookies=cookies, headers=headers)
+if response.status_code==200:
+    print("Logged Out Successfully")
+else:
+    print("Error Logging Out")
 
-response = requests.request("POST", url, data=payload, headers=headers)
-print(response.text)
+print("Exiting")
